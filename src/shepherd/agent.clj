a70bd3e4de3584d5c719309acd4cb2bddf323fee
Ryan Spangler <spanglry@stanford.edu>
2018-11-08 16:35:02 -0800

    starting on agent namespace

--- src/shepherd/agent.clj
+++ src/shepherd/agent.clj
@@ -1,4 +1,15 @@
-(ns shepherd.agent)
+(ns shepherd.agent
+  (:require
+   [cheshire.core :as json]
+   [shepherd.process :as process]))
+
+(defn launch-agent
+  [agent-spec]
+  (process/launch
+   ["python" "-m" "environment.boot"
+    "--id" (:agent-id agent-spec)
+    "--type" (:agent-type agent-spec)
+    "--config" (json/generate-string (:agent-config agent-spec))]))
 
 (defn add-agent
-  [agent-id agent-type agent-config])
+  [shepherd kafka agent-spec])
