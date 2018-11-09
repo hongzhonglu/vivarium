388b7cc7c2e5232240e12e404155b412173eea76
Ryan Spangler <ryan.spangler@gmail.com>
2018-11-09 01:01:40 -0800

    flow version

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismofeverything/flow "0.0.1"]]
+                 [prismofeverything/flow "0.0.2"]]
   :main shepherd.core)
