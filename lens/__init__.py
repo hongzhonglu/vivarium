5f93daf398bb7148627214adf04b25113cdfdc32
Eran Agmon <agmon.eran@gmail.com>
2019-07-15 14:09:46 -0700

    add Outer, Inner, BootAgent to namespace

---
+++ lens/__init__.py
@@ -0,0 +1,3 @@
+from agent.outer import Outer
+from agent.inner import Inner
+from agent.boot import BootAgent
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", 'r') as requirements:
 
 setup(
 	name='wholecell-lens',
-	version='0.0.1',
+	version='0.0.2',
 	packages=['lens'],
 	author='Eran Agmon',
 	author_email='eagmon@stanford.edu',
