e8ab87374bce66c9e0b4cb5a248ded32dbe5afea
Eran Agmon <agmon.eran@gmail.com>
2019-09-20 11:46:20 -0700

    towards function

--- lens/utils/regulation_logic.py
+++ lens/utils/regulation_logic.py
@@ -42,27 +42,38 @@ class LogicConstructor(NodeVisitor):
     '''
     def visit_rule(self, node, visited_children):
         if_statement, sets_mols, = visited_children
-        rule = []
+        rule_string = 'if '
         for set_mols in sets_mols:
-            for mol in set_mols:
-                rule.append(mol)
-        return rule
+            for mol_operation in set_mols:
+                operation, mol = mol_operation
+                if operation:
+                    mol_dict = '{} dict.get({}, False)'.format(operation, mol)
+                else:
+                    mol_dict = 'dict.get({}, False)'.format(operation, mol)
+                rule_string = rule_string + mol_dict + ' '
+        return rule_string
     def visit_set_mols(self, node, visited_children):
-        _, molecules, _, = visited_children
+        open, molecules, close = visited_children
+
+        # import ipdb;
+        # ipdb.set_trace()
+
+
         return molecules
     def visit_one_molecule(self, node, visited_children):
-        operation, mol_id = visited_children
-        return mol_id
+        operation_list, mol_id = visited_children
+        operation = operation_list[0]  # TODO -- can this be take out of list before?
+        return (operation, mol_id)
     def visit_text(self, node, visited_children):
         return (node.text)
     def visit_operation(self, node, visited_children):
         or_oper, and_oper, not_oper = visited_children
         if isinstance(or_oper, list):
-            return 'or'
+            return ('or')
         elif isinstance(and_oper, list):
-            return 'and'
+            return ('and')
         elif isinstance(not_oper, list):
-            return 'not'
+            return ('not')
     def visit_or(self, node, visited_children):
         return True
     def visit_and(self, node, visited_children):
@@ -72,9 +83,9 @@ class LogicConstructor(NodeVisitor):
     def visit_if(self, node, visited_children):
         pass
     def visit_open(self, node, visited_children):
-        pass
+        return ('(')
     def visit_close(self, node, visited_children):
-        pass
+        return (')')
     def visit_ws(self, node, visited_children):
         pass
     def generic_visit(self, node, visited_children):
@@ -84,13 +95,30 @@ class LogicConstructor(NodeVisitor):
 
 
 
-str = "IF (GLCxt or LCTSxt or RIBxt or GLxt or LACxt or PYRxt or SUCCxt or ETHxt or ACxt or FORxt)"
-# str = "IF not (GLCxt or LCTSxt or RUBxt) and FNR and not GlpR"
+# str = "IF (GLCxt or LCTSxt or RIBxt or GLxt or LACxt or PYRxt or SUCCxt or ETHxt or ACxt or FORxt)"
+str = "IF not (GLCxt or LCTSxt or RUBxt) and FNR and not GlpR"
 
 # str = "IF GLCxt or LCTSxt"
 
 str_parsed = grammar.parse(str)
 lc = LogicConstructor()
 logic = lc.visit(str_parsed)
+
+# import ipdb; ipdb.set_trace()
+
+
+
+def test_rule(dict):
+    return (dict.get('GLCxt', False) or dict.get('LCTSxt', False)) and dict.get('FNR', False) and not dict.get('GlpR', False)
+
+
+state = {
+    'GLCxt': False,
+    'LCTSxt': True,
+    'FNR': True,
+    'GlpR': True,
+}
+
+result = test_rule(state)
 
 import ipdb; ipdb.set_trace()
