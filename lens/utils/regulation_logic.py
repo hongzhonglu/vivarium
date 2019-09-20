e3ae94bb10dc2c426257434d598a1ba96c1ee658
Eran Agmon <agmon.eran@gmail.com>
2019-09-20 10:46:31 -0700

    partially working parsing expression grammar for regulation

--- lens/utils/regulation_logic.py
+++ lens/utils/regulation_logic.py
@@ -5,17 +5,17 @@ from parsimonious.nodes import NodeVisitor
 
 grammar = Grammar(
     """
-    rule = if one_molecule
-    one_molecule = text ws? and? or? not?
-
-    text = ~"[A-Z 0-9]*"i
-
-    if = ws? "IF" ws?
-    and = ws? "and" ws?
-    or = ws? "or" ws?
-    not = ws? "not" ws?
+    rule = if set_mols*
+    set_mols = open? one_molecule* close?
+    one_molecule = operation? text
+    text = ~"[A-Za-z0-9]*"i
+    if = "IF" ws
     open  = "("
     close = ")"
+    operation = or? and? not?
+    or = ws "or" ws?
+    and = ws "and" ws?
+    not = ws "not" ws?
     ws = ~"\s*"
     """)
 
@@ -41,19 +41,35 @@ class LogicConstructor(NodeVisitor):
         - visited_children: The results of visiting the children of that node, in a list
     '''
     def visit_rule(self, node, visited_children):
-        pass
+        if_statement, sets_mols, = visited_children
+        rule = []
+        for set_mols in sets_mols:
+            for mol in set_mols:
+                rule.append(mol)
+        return rule
+    def visit_set_mols(self, node, visited_children):
+        _, molecules, _, = visited_children
+        return molecules
     def visit_one_molecule(self, node, visited_children):
-        import ipdb; ipdb.set_trace()
-        pass
+        operation, mol_id = visited_children
+        return mol_id
     def visit_text(self, node, visited_children):
-        pass
-    def visit_if(self, node, visited_children):
-        pass
-    def visit_and(self, node, visited_children):
-        pass
+        return (node.text)
+    def visit_operation(self, node, visited_children):
+        or_oper, and_oper, not_oper = visited_children
+        if isinstance(or_oper, list):
+            return 'or'
+        elif isinstance(and_oper, list):
+            return 'and'
+        elif isinstance(not_oper, list):
+            return 'not'
     def visit_or(self, node, visited_children):
-        pass
+        return True
+    def visit_and(self, node, visited_children):
+        return True
     def visit_not(self, node, visited_children):
+        return True
+    def visit_if(self, node, visited_children):
         pass
     def visit_open(self, node, visited_children):
         pass
@@ -68,11 +84,10 @@ class LogicConstructor(NodeVisitor):
 
 
 
-# str = "IF (GLCxt or LCTSxt or RIBxt or GLxt or LACxt or PYRxt or SUCCxt or ETHxt or ACxt or FORxt)"
-# str2 = "IF not (GLCxt or LCTSxt or RUBxt) and FNR and not GlpR"
+str = "IF (GLCxt or LCTSxt or RIBxt or GLxt or LACxt or PYRxt or SUCCxt or ETHxt or ACxt or FORxt)"
+# str = "IF not (GLCxt or LCTSxt or RUBxt) and FNR and not GlpR"
 
-str = "IF GLCxt"
-
+# str = "IF GLCxt or LCTSxt"
 
 str_parsed = grammar.parse(str)
 lc = LogicConstructor()
