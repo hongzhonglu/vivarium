df73a5449ff368a07e26b87dab9613dc80d85830
Ryan Spangler <spanglry@stanford.edu>
2019-10-03 14:23:28 -0700

    using matplotlibrc to set backend, catching exception in nf_cplex (for pytest)

--- lens/analysis/analyze_compartment.py
+++ lens/analysis/analyze_compartment.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function
 import os
 
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
