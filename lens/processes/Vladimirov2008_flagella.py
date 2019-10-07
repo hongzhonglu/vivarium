99f82ba590ccdad6826f1f15f91eca1fd927fb81
Eran Agmon <agmon.eran@gmail.com>
2019-10-07 15:11:15 -0700

    clean up flagella

--- lens/processes/Vladimirov2008_flagella.py
+++ lens/processes/Vladimirov2008_flagella.py
@@ -9,24 +9,27 @@ from lens.actor.process import Process
 TUMBLE_JITTER = 0.5  # (radians)
 
 ## Parameters
-k_A = 5.0  #
-k_y = 100.0  # 1/uM/s
-k_z = 30.0  # / self.CheZ,
-gamma_Y = 0.1
-k_s = 0.45  # scaling coefficient
+DEFAULT_PARAMETERS = {
+    'k_A': 5.0,  #
+    'k_y': 100.0,  # 1/uM/s
+    'k_z': 30.0,  # / self.CheZ,
+    'gamma_Y': 0.1,
+    'k_s': 0.45,  # scaling coefficient
 
-receptor_adapt_rate = 1000
-adaptPrecision = 0.3  # set to 1.0 for perfect adaptation
+    'receptor_adapt_rate': 1000,
+    'adaptPrecision': 0.3,  # set to 1.0 for perfect adaptation
+
+    # motor
+    'mb_0': 0.65,
+    'n_motors': 5,
+    'cw_to_ccw': 0.83,  # 1/s (Block1983) motor bias, assumed to be constant
+}
 
 # initial concentrations
-CheR_WT = 0.00016  # (mM) wild type concentration. 0.16 uM = 0.00016 mM
-CheB_WT = 0.00028  # (mM) wild type concentration. 0.28 uM = 0.00028 mM
-
-# motor
-mb_0 = 0.65
-n_motors = 5
-cw_to_ccw = 0.83  # 1/s (Block1983) motor bias, assumed to be constant
-
+initial = {
+    'CheR_WT': 0.00016,  # (mM) wild type concentration. 0.16 uM = 0.00016 mM
+    'CheB_WT': 0.00028,  # (mM) wild type concentration. 0.28 uM = 0.00028 mM
+}
 
 class FlagellaActivity(Process):
     def __init__(self, initial_parameters={}):
@@ -35,7 +38,7 @@ class FlagellaActivity(Process):
             'internal': [],
             'external': []
         }
-        parameters = {}
+        parameters = DEFAULT_PARAMETERS
         parameters.update(initial_parameters)
 
         super(FlagellaActivity, self).__init__(roles, parameters, deriver=True)
@@ -47,9 +50,12 @@ class FlagellaActivity(Process):
             - internal (dict) -- internal states with default initial values
         '''
 
+        internal = {'chemoreceptor_P_on': 0.5,
+                    'motor_state': 'tumble'}
+
         return {
             'external': {},
-            'internal': {}}
+            'internal': internal}
 
     def default_emitter_keys(self):
         keys = {
@@ -73,67 +79,62 @@ class FlagellaActivity(Process):
 
         internal = states['internal']
         external = states['external']
-
-        # update receptor clusters and get their probability of being active
-        # for cluster in self.receptor_clusters:
-        #     cluster.update(ligand_conc, self.CheR, self.CheB)
-        clusters_p_on = [cluster.P_on for cluster in self.receptor_clusters]
-        P_on = sum(clusters_p_on) / self.n_receptors_clusters
+        P_on = internal['chemoreceptor_P_on']
+        motor_state = internal['motor_state']
+        CheZ = internal['CheZ']
+        CheY_tot = internal['CheY_tot']
 
         # CheY phosphorylation, scaled to 1.0 at rest
         # self.CheA_P = self.CheA_tot * self.P_on * k_A / (self.P_on * k_A + k_y * self.CheY_tot)  # amount of phosphorylated CheA
         # scaling = 19.3610  # scales CheY_P linearly so that CheY_P=1 at rest (P_on=1/3)
         # self.CheY_P = adaptPrecision * scaling * self.CheY_tot * k_y * self.CheA_P / (k_y * self.CheA_P + k_z * self.CheZ + gamma_Y)
-        self.CheY_P = adaptPrecision * 3 * k_y * self.CheY_tot * P_on * k_s / (k_y * k_s * P_on + k_z * self.CheZ + gamma_Y)
+        CheY_P = self.parameters['adaptPrecision'] * 3 * self.parameters['k_y'] * CheY_tot * P_on * self.parameters['k_s'] / \
+                 (self.parameters['k_y'] * self.parameters['k_s'] * P_on + self.parameters['k_z'] * CheZ + self.parameters['gamma_Y'])
 
         # CheB phosphorylation
         # TODO!
 
 
-
         ## Motor switching
         # CCW corresponds to run. CW corresponds to tumble
-        ccw_motor_bias = mb_0 / (self.CheY_P/self.CheY_tot * (1 - mb_0) + mb_0)
-        cww_to_cw = cw_to_ccw * (1 / ccw_motor_bias - 1)
+        ccw_motor_bias = self.parameters['mb_0'] / (CheY_P/CheY_tot * (1 - self.parameters['mb_0']) + self.parameters['mb_0'])
+        cww_to_cw = self.parameters['cw_to_ccw'] * (1 / ccw_motor_bias - 1)
 
-        # print("current state: " + str(self.motor_state))
-        # print("ccw_motor_bias: " + str(ccw_motor_bias))
-        # print("cw_to_ccw: " + str(cw_to_ccw))
-        # print("cww_to_cw: " + str(cww_to_cw))
 
-        if self.motor_state is 'run':
+        if motor_state is 'run':
             # switch to tumble?
-            prob_switch = cww_to_cw * self.timestep
+            prob_switch = cww_to_cw * timestep
             print('prob_switch ' + str(prob_switch))
             if prob_switch <= np.random.random(1)[0]:
-                self.motor_state = 'tumble'
-                self.tumble()
+                motor_state = 'tumble'
+                motile_force = self.tumble()
             else:
-                self.run()
+                motile_force = self.run()
 
-        elif self.motor_state is 'tumble':
+        elif motor_state is 'tumble':
             # switch to run?
-            prob_switch = cw_to_ccw * self.timestep
+            prob_switch = self.parameters['cw_to_ccw'] * timestep
             print('prob_switch ' + str(prob_switch))
             if prob_switch <= np.random.random(1)[0]:
-                self.motor_state = 'run'
-                self.run()
+                motor_state = 'run'
+                motile_force = self.run()
             else:
-                self.tumble()
+                motile_force = self.tumble()
 
 
-        update = {}
+        update = {
+            'motile_force': motile_force,
+            'motor_state': motor_state,
+            'CheY_P': CheY_P}
 
         return update
 
     def tumble(self):
         force = 5.0
         torque = random.normalvariate(0, TUMBLE_JITTER)
-        self.motile_force = [force, torque]
-        # print('TUMBLE!')
+        return [force, torque]
 
     def run(self):
         force = 15.0
         torque = 0.0
-        self.motile_force = [force, torque]
-        # print('RUN!')
+        return [force, torque]
