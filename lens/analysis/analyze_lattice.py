31fd7faa93442af5be1c05c83f341bd1f05bca9e
Eran Agmon <agmon.eran@gmail.com>
2019-10-28 17:06:15 -0700

    speed up lattice analysis

--- lens/analysis/analyze_lattice.py
+++ lens/analysis/analyze_lattice.py
@@ -23,20 +23,18 @@ class LatticeTrace(Analysis):
         time_vec = [t / 3600 for t in history_data['time']]  # convert to hours
         edge_length = experiment_config['edge_length']
 
+        # plot trajectories
         plt.figure(figsize=(8, 8))
         for agent_id in agent_ids:
             # get locations and convert to 2D array
             locations = history_data[agent_id]['location']
             locations_array = np.array(locations)
-
             x_coord = locations_array[:, 0]
             y_coord = locations_array[:, 1]
-            angle = locations_array[:, 2]
 
-            # mark starting point
-            plt.plot(x_coord[0], y_coord[0], 'r*')
-            for i in range(len(locations)):
-                plt.plot(x_coord[i:i + 2], y_coord[i:i + 2], 'b-')
+            plt.plot(x_coord, y_coord, 'b-')  # trajectory
+            plt.plot(x_coord[0], y_coord[0], color=(0.0,0.8,0.0), marker='*')  # starting point
+            plt.plot(x_coord[-1], y_coord[-1], color='r', marker='*')  #  ending point
 
         plt.xlim((0, edge_length))
         plt.ylim((0, edge_length))
