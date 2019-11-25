46cfbfaa59eb0cb4d270672851162d439b633a22
Ryan Spangler <spanglry@stanford.edu>
2019-11-25 15:54:14 -0800

    getting flux through networks

--- lens/processes/cobra_metabolism.py
+++ lens/processes/cobra_metabolism.py
@@ -9,6 +9,8 @@ from cobra import Model, Reaction, Metabolite, Configuration
 from lens.actor.process import Process, deep_merge
 from lens.utils.units import units
 
+EXTERNAL_SUFFIX = '_external'
+
 def get_reverse(stoichiometry, reversible_reactions, reverse_key):
     '''
     stoichiometry (dict) -- {mol_id (str): stoich (dict)}
@@ -21,7 +23,7 @@ def get_reverse(stoichiometry, reversible_reactions, reverse_key):
             mol_id: -1 * coeff for mol_id, coeff in forward_stoich.items()}
     return reverse_stoichiometry
 
-def build_model(stoichiometry, objective):
+def build_model(stoichiometry, objective, external_molecules):
     model = Model('metabolism')
     model.compartments = {'c': 'cytoplasm'}
 
@@ -38,12 +40,19 @@ def build_model(stoichiometry, objective):
     reactions = {}
     for reaction_key, chemistry in stoichiometry.items():
         reaction = Reaction(reaction_key, name=reaction_key)
-        chemistry_model = {
+        reaction_model = {
             metabolites[metabolite]: value
             for metabolite, value in chemistry.items()}
-        reaction.add_metabolites(chemistry_model)
-        reaction.gene_reaction_rule = 'X'
+        reaction.add_metabolites(reaction_model)
+        # reaction.gene_reaction_rule = 'X'
         reactions[reaction_key] = reaction
+
+    for external in external_molecules:
+        external_key = external + EXTERNAL_SUFFIX
+        reaction = Reaction(external_key, name=external_key)
+        reaction_model = {metabolites[external]: -1}
+        reaction.add_metabolites(reaction_model)
+        reactions[external_key] = reaction
 
     model.add_reactions(reactions.values())
 
@@ -62,6 +71,7 @@ class CobraMetabolism(Process):
 
         self.initial_state = initial_parameters['initial_state']
         self.stoichiometry = initial_parameters['stoichiometry']
+        self.external_molecules = initial_parameters['external_molecules']
 
         self.reverse_key = '_reverse'
         self.reversible_reactions = initial_parameters.get('reversible_reactions', [])
@@ -72,59 +82,77 @@ class CobraMetabolism(Process):
         self.stoichiometry.update(self.objective)
 
         self.objective_declaration = {reaction: 1.0 for reaction in self.objective.keys()}
-        self.model = build_model(self.stoichiometry, self.objective_declaration)
+        self.model = build_model(
+            self.stoichiometry,
+            self.objective_declaration,
+            self.external_molecules)
+
+    def set_external_levels(self, levels):
+        for external, level in levels.items():
+            reaction = self.model.reactions.get_by_id(external + EXTERNAL_SUFFIX)
+            reaction.lower_bound = -level
+
 
 def test_minimal():
     stoichiometry = {
-        "R1": {"A": -1, "B": 1}}
+        # 'EA': {'A': 1},
+        'R1': {'A': -1, 'B': 1}}
 
     objective = {
-        "R2": {"B": -1, "C": 1}}
+        'EB': {'B': -1}}
+
+    external_molecules = ['A']
 
     initial_state = {
-        "A": 5,
-        "B": 0,
-        "C": 0}
+        'A': 5}
 
     metabolism = CobraMetabolism({
         'stoichiometry': stoichiometry,
         'objective': objective,
+        'external_molecules': external_molecules,
         'initial_state': initial_state})
+
+    metabolism.set_external_levels(initial_state)
 
     return metabolism
 
 def test_metabolism():
     stoichiometry = {
-        "R1": {"A": -1, "ATP": -1, "B": 1},
-        "R2a": {"B": -1, "ATP": 2, "NADH": 2, "C": 1},
-        "R2b": {"C": -1, "ATP": -2, "NADH": -2, "B": 1},
-        "R3": {"B": -1, "F": 1},
-        "R4": {"C": -1, "G": 1},
-        "R5": {"G": -1, "C": 0.8, "NADH": 2},
-        "R6": {"C": -1, "ATP": 2, "D": 3},
-        "R7": {"C": -1, "NADH": -4, "E": 3},
-        "R8a": {"G": -1, "ATP": -1, "NADH": -2, "H": 1},
-        "R8b": {"G": 1, "ATP": 1, "NADH": 2, "H": -1},
-        "Rres": {"NADH": -1, "O2": -1, "ATP": 1}}
+        'R1': {'A': -1, 'ATP': -1, 'B': 1},
+        'R2a': {'B': -1, 'ATP': 2, 'NADH': 2, 'C': 1},
+        'R2b': {'C': -1, 'ATP': -2, 'NADH': -2, 'B': 1},
+        'R3': {'B': -1, 'F': 1},
+        'R4': {'C': -1, 'G': 1},
+        'R5': {'G': -1, 'C': 0.8, 'NADH': 2},
+        'R6': {'C': -1, 'ATP': 2, 'D': 3},
+        'R7': {'C': -1, 'NADH': -4, 'E': 3},
+        'R8a': {'G': -1, 'ATP': -1, 'NADH': -2, 'H': 1},
+        'R8b': {'G': 1, 'ATP': 1, 'NADH': 2, 'H': -1},
+        'Rres': {'NADH': -1, 'O2': -1, 'ATP': 1}}
 
-    objective = {"v_biomass": {"C": -1, "F": -1, "H": -1, "ATP": -10, "BIOMASS": 1}}
+    external_molecules = ['A', 'F', 'D', 'E', 'H', 'O2', 'BIOMASS']
+
+    objective = {'v_biomass': {'C': -1, 'F': -1, 'H': -1, 'ATP': -10, 'BIOMASS': 1}}
 
     initial_state = {
         'internal': {
             'mass': 1339,
             'volume': 1E-15},
         'external': {
-            "A": 21.0,
-            "F": 5.0,
-            "D": 12.0,
-            "E": 12.0,
-            "H": 5.0,
-            "O2": 100.0}}
+            'A': 21.0,
+            'F': 5.0,
+            'D': 12.0,
+            'E': 12.0,
+            'H': 5.0,
+            'O2': 100.0}}
 
     metabolism = CobraMetabolism({
         'stoichiometry': stoichiometry,
         'objective': objective,
+        'external_molecules': external_molecules,
         'initial_state': initial_state})
+
+    metabolism.set_external_levels(initial_state['external'])
 
     return metabolism
 
@@ -194,24 +222,24 @@ def test_demo():
     print('%i genes' % len(model.genes))
 
     # Iterate through the the objects in the model
-    print("Reactions")
-    print("---------")
+    print('Reactions')
+    print('---------')
     for x in model.reactions:
-        print("%s : %s" % (x.id, x.reaction))
+        print('%s : %s' % (x.id, x.reaction))
 
-    print("")
-    print("Metabolites")
-    print("-----------")
+    print('')
+    print('Metabolites')
+    print('-----------')
     for x in model.metabolites:
         print('%9s : %s' % (x.id, x.formula))
 
-    print("")
-    print("Genes")
-    print("-----")
+    print('')
+    print('Genes')
+    print('-----')
     for x in model.genes:
         associated_ids = (i.id for i in x.reactions)
-        print("%s is associated with reactions: %s" %
-              (x.id, "{" + ", ".join(associated_ids) + "}"))
+        print('%s is associated with reactions: %s' %
+              (x.id, '{' + ', '.join(associated_ids) + '}'))
 
     model.objective = '3OAS140'
 
@@ -225,11 +253,11 @@ def test_demo():
     return DemoFBA(model)
 
 if __name__ == '__main__':
-    # metabolism = test_metabolism()
+    metabolism = test_metabolism()
     # metabolism = test_minimal()
     # metabolism = test_canonical()
     # metabolism = test_demo()
-    metabolism = test_test()
+    # metabolism = test_test()
 
     # cobra.io.save_json_model(metabolism.model, 'demo_model.json')
 
