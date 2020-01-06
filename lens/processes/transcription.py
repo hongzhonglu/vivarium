e61d01b623adfeedd28b769945bc0df37437f512
Ryan Spangler <ryan.spangler@gmail.com>
2020-01-06 10:00:46 -0800

    starting transcription process

---
+++ lens/processes/transcription.py
@@ -0,0 +1,10 @@
+from lens.actor.process import Process
+
+class Transcription(Process):
+    def __init__(self, initial_parameters={}):
+        self.promoter_affinities = initial_parameters.get('promoter_affinities', {})
+
+    def next_update(self, timestep, states):
+        pass
+
+        
