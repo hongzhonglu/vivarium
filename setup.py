3344e70f4db3e85b9d6b08ee931fa79ad60beb10
Ryan Spangler <spanglry@stanford.edu>
2020-01-08 13:26:54 -0800

    version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", 'r') as requirements:
 
 setup(
     name='wholecell-vivarium',
-    version='0.0.29',
+    version='0.0.30',
     packages=[
         'vivarium',
         'vivarium.actor',
