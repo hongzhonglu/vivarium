ca5d04eb5c141d4df56213d59c42c99891391bfb
Eran Agmon <agmon.eran@gmail.com>
2020-01-28 13:04:20 -0800

    move test chromosome to data/chromosome.py

--- vivarium/states/chromosome.py
+++ vivarium/states/chromosome.py
@@ -2,6 +2,8 @@ import random
 import copy
 import numpy as np
 
+from vivarium.data.chromosome import test_chromosome_config
+
 def first(l):
     if l:
         return l[0]
@@ -495,71 +497,6 @@ class Chromosome(Datum):
         self.rnap_id = 0
 
 
-test_chromosome_config = {
-    'sequence': 'ATACGGCACGTGACCGTCAACTTA',
-    'genes': {
-        'oAZ': ['A', 'Z'],
-        'oA': ['A'],
-        'oB': ['B']},
-    'promoters': {
-        'pA': {
-            'id': 'pA',
-            'position': 3,
-            'direction': 1,
-            'sites': [{
-                'position': 0,
-                'length': 3,
-                'thresholds': [
-                    ('tfX', 0.3)]}],
-            'terminators': [
-                {
-                    'position': 6,
-                    'strength': 0.5,
-                    'operon': 'oA'},
-                {
-                    'position': 11,
-                    'strength': 1.0,
-                    'operon': 'oAZ'}]},
-        'pB': {
-            'id': 'pB',
-            'position': -3,
-            'direction': -1,
-            'sites': [{
-                'position': 0,
-                'length': 3,
-                'thresholds': [
-                    ('tfX', 0.5)]}],
-            'terminators': [
-                {
-                    'position': -8,
-                    'strength': 0.5,
-                    'operon': 'oB'},
-                {
-                    'position': -11,
-                    'strength': 1.0,
-                    'operon': 'oBY'}]}},
-    'domains': {
-        0: {
-            'id': 0,
-            'lead': 0,
-            'lag': 0,
-            'children': []}},
-    'rnaps': [
-        {
-            'promoter': 'pA',
-            'domain': 0,
-            'state': 'transcribing',
-            'position': 3},
-        {
-            'promoter': 'pA',
-            'domain': 0,
-            'state': 'transcribing',
-            'position': 6},
-        {
-            'promoter': 'pA',
-            'domain': 0,
-            'state': 'transcribing',
-            'position': 0}]}
 
 def test_chromosome():
     chromosome = Chromosome(test_chromosome_config)
