e97f1a9ed921a8dfccdc71c62b24b3fc370f9fca
Ryan Spangler <spanglry@stanford.edu>
2020-02-19 15:17:31 -0800

    filling out flagella chromosome

--- vivarium/data/chromosomes/flagella_chromosome.py
+++ vivarium/data/chromosomes/flagella_chromosome.py
@@ -2,17 +2,226 @@ from vivarium.data.knowledge_base import KnowledgeBase
 
 knowledge_base = KnowledgeBase()
 
+genes_list = knowledge_base.wcEcoli_genes
+genes = {
+    gene['symbol']: gene
+    for gene in genes_list}
 
-
-
-
-flagella_genes = knowledge_base.wcEcoli_genes
 flagella_sequence = ''
+
+import ipdb; ipdb.set_trace()
 
 flagella_config = {
     'sequence': flagella_sequence,
-    'genes': {},
-    'promoters': {},
+    'genes': {
+        'flhDC': ['flhD', 'flhC'],
+        'fliL': ['fliL', 'fliM', 'fliN', 'fliO', 'fliP', 'fliQ', 'fliR'],
+        'fliE': ['fliE'],
+        'fliF': ['fliF', 'fliG', 'fliH', 'fliI', 'fliJ', 'fliK'],
+        'flgA': ['flgA', 'flgM', 'flgN'],
+        'flgB': ['flgB', 'flgC', 'flgD', 'flgE', 'flgF', 'flgG', 'flgH', 'flgI', 'flgJ'],
+        'flhB': ['flhB', 'flhA', 'flhE'],
+        'fliA': ['fliA', 'fliZ', 'tcyJ'],
+        'fliD': ['fliD', 'fliS', 'fliT'],
+        'flgK': ['flgK', 'flgL'],
+        'fliC': ['fliC'],
+        'meche': [],
+        'mocha': [],
+        'flgM': ['flgM', 'flgN']},
+    'promoters': {
+        'flhDp': {
+            'id': 'flhDp',
+            'position': 1978197,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('CRP', 0.1)]}],
+            'terminators': [
+                {
+                    'position': 1977266,
+                    'strength': 1.0,
+                    'products': ['flhDC']}]},
+        'fliLp1': {
+            'id': 'fliLp1',
+            'position': 2019618,
+            'direction': 1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.1)]}],
+            'terminators': [
+                {
+                    'position': 2023678,
+                    'strength': 1.0,
+                    'products': ['fliL']}]},
+        'fliEp1': {
+            'id': 'fliEp1',
+            'position': 2013014,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.15)]}],
+            'terminators': [
+                {
+                    'position': 2012700,
+                    'strength': 1.0,
+                    'products': ['fliE']}]},
+        'fliFp1': {
+            'id': 'fliFp1',
+            'position': 2013229,
+            'direction': 1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.2)]}],
+            'terminators': [
+                {
+                    'position': 2019513,
+                    'strength': 1.0,
+                    'products': ['fliF']}]},
+        'flgAp': {
+            'id': 'flgAp',
+            'position': 1130863,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.25)]}],
+            'terminators': [
+                {
+                    'position': 1129414,
+                    'strength': 1.0,
+                    'products': ['flgA']}]},
+        'flgBp': {
+            'id': 'flgBp',
+            'position': 1131018,
+            'direction': 1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.3)]}],
+            'terminators': [
+                {
+                    'position': 1138312,
+                    'strength': 1.0,
+                    'products': ['flgB']}]},
+        'flhBp': {
+            'id': 'flhBp',
+            'position': 1966191,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.35)]}],
+            'terminators': [
+                {
+                    'position': 1962580,
+                    'strength': 1.0,
+                    'products': ['flhB']}]},
+        'fliAp1': {
+            'id': 'fliAp1',
+            'position': 2001789,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.4)]}],
+            'terminators': [
+                {
+                    'position': 1999585,
+                    'strength': 1.0,
+                    'products': ['fliA']}]},
+        'fliDp': {
+            'id': 'fliDp',
+            'position': 2003872,
+            'direction': 1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.45)]}],
+            'terminators': [
+                {
+                    'position': 2006078,
+                    'strength': 1.0,
+                    'products': ['fliD']}]},
+        'flgKp': {
+            'id': 'flgK',
+            'position': 1138378,
+            'direction': 1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('flhDC', 0.5)]}],
+            'terminators': [
+                {
+                    'position': 1140986,
+                    'strength': 1.0,
+                    'products': ['flgK']}]},
+        'fliC': {
+            'id': 'fliC',
+            'position': 2002110,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('GadE', 0.55),
+                        ('H-NS', 0.6)]}],
+            'terminators': [
+                {
+                    'position': 2003606,
+                    'strength': 1.0,
+                    'products': ['fliC']}]},
+        'meche': {
+            'id': 'meche',
+            'position': 0,
+            'direction': 1,
+            'sites': [{}],
+            'terminators': [{}]},
+        'mocha': {
+            'id': 'mocha',
+            'position': 0,
+            'direction': 1,
+            'sites': [{}],
+            'terminators': [{}]},
+        'flgMp': {
+            'id': 'flgMp',
+            'position': 1130128,
+            'direction': -1,
+            'sites': [
+                {
+                    'position': 0,
+                    'length': 0,
+                    'thresholds': [
+                        ('CsgD', 0.1)]}],
+            'terminators': [
+                {
+                    'position': 1129414,
+                    'strength': 1.0,
+                    'products': ['flgM']}]}},
     'domains': {
         0: {
             'id': 0,
