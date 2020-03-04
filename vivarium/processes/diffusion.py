f75df8f1d0bdbb979d125ecc7a10b767363f3a72
Eran Agmon <agmon.eran@gmail.com>
2020-03-03 20:09:12 -0800

    make ports by coordinates

--- vivarium/processes/diffusion.py
+++ vivarium/processes/diffusion.py
@@ -13,9 +13,6 @@ from vivarium.compartment.composition import (
     convert_to_timeseries,
     plot_simulation_output)
 
-# constants
-DIFFUSION_CONSTANT = 1e3
-
 # laplacian kernel for diffusion
 LAPLACIAN_2D = np.array([[0.0, 1.0, 0.0], [1.0, -4.0, 1.0], [0.0, 1.0, 0.0]])
 
@@ -27,51 +24,63 @@ class Diffusion(Process):
     def __init__(self, initial_parameters={}):
 
         # locations
-        self.fields = initial_parameters.get('fields', [])
+        self.molecules = initial_parameters.get('molecules', {'glc': 1})
+        self.molecule_ids = list(self.molecules.keys())
         self.membranes = initial_parameters.get('membranes', [])
 
         # diffusion parameters
-        self.edge_length_x = 2
-        self.patches_per_edge_x = 2
-        self.edge_length_y = 1
-        self.patches_per_edge_y = 1
+        self.length_x = initial_parameters.get('length_x', 2)
+        self.bins_x = initial_parameters.get('bins_x', 2)
+        self.length_y = initial_parameters.get('length_y', 1)
+        self.bins_y = initial_parameters.get('bins_y', 1)
+        self.diffusion = initial_parameters.get('diffusion', 1e3)
 
-        self.diffusion = initial_parameters.get('diffusion', DIFFUSION_CONSTANT)
-
-        self.dx = self.edge_length_x / self.patches_per_edge_x
-        self.dy = self.edge_length_y / self.patches_per_edge_y
+        self.dx = self.length_x / self.bins_x
+        self.dy = self.length_y / self.bins_y
         self.dx2 = self.dx * self.dy
         self.diffusion_dt = 0.5 * self.dx ** 2 * self.dy ** 2 / (2 * self.diffusion * (self.dx ** 2 + self.dy ** 2))
 
-        # # get initial state
-        # states = list(self.transcription.keys()) + list(self.translation.keys())
-        # null_states = {'internal': {
-        #     state_id: 0 for state_id in states}}
-        # initialized_states = initial_parameters.get('initial_state', {})
-        # self.initial_state = deep_merge(null_states, initialized_states)
-        # internal = list(self.initial_state.get('internal', {}).keys())
-        # external = list(self.initial_state.get('external', {}).keys())
+        # make fields
+        fields = self.fill_fields(self.molecules)
 
-        import ipdb; ipdb.set_trace()
-
+        # make ports
         ports = {}
+        for x in range(self.bins_x):
+            for y in range(self.bins_y):
+                concs = {
+                    mol_id: fields[mol_index][x][y]
+                    for mol_index, mol_id in enumerate(self.molecule_ids)}
+                ports[(x,y)]= concs
 
         parameters = {}
         parameters.update(initial_parameters)
 
         super(Diffusion, self).__init__(ports, parameters)
 
+    def fill_fields(self, molecules={}):
+        # Create lattice and fill each site with concentrations dictionary
+        # Molecule identities are defined along the major axis, with spatial dimensions along the other two axes.
+
+
+        fields = np.empty((len(self.molecule_ids), self.bins_x, self.bins_y), dtype=np.float64)
+        for index, molecule_id in enumerate(self.molecule_ids):
+            fields[index].fill(molecules[molecule_id])
+        return fields
+
     def default_settings(self):
-        default_settings = {}
+        default_settings = {
+            'state': {},
+        }
         return default_settings
 
     def next_update(self, timestep, states):
-        internal_state = states['internal']
+        import ipdb; ipdb.set_trace()
+
 
         update = {}
         return update
 
-    # diffusion ufnctions
+    # diffusion functions
     def diffusion_timestep(self, field, dt):
         ''' calculate concentration changes cause by diffusion'''
         change_field = self.diffusion * dt * convolve(field, LAPLACIAN_2D, mode='reflect') / self.dx2
@@ -90,6 +99,8 @@ class Diffusion(Process):
 
 # testing
 def get_cell_config():
+
+
     return {}
 
 def test_diffusion(time=10):
