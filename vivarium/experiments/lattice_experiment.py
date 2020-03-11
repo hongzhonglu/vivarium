d25f196c19d37e1660aef14512fb16db40e99ec5
Eran Agmon <agmon.eran@gmail.com>
2020-03-11 13:13:51 -0700

    add a standard boot config to lattice_experiment

--- vivarium/experiments/lattice_experiment.py
+++ vivarium/experiments/lattice_experiment.py
@@ -26,10 +26,29 @@ def compose_lattice_experiment(config):
     ipdb.set_trace()
 
 
+def get_ecoli_core_glc_config():
+    timeline_str = '0 ecoli_core_GLC 1.0 L + lac__D_e 1.0 mmol 0.1 L, 21600 end'
+    lattice_config = {
+        'name': 'ecoli_core',
+        'timeline_str': timeline_str,
+        'edge_length_x': 15.0,
+        'patches_per_edge_x': 10,
+        'run_for': 5.0,
+        'diffusion': 1e-3,
+        'depth': 2e-2,
+        'translation_jitter': 1e-1,
+        'emit_fields': [
+            'co2_e',
+            'o2_e',
+            'glc__D_e',
+            'lac__D_e']}
+
+    return lattice_config
+
 if __name__ == '__main__':
     out_dir = os.path.join('out', 'tests', 'lattice_experiment')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    config = {}
+    config = get_ecoli_core_glc_config()
     experiment = compose_lattice_experiment(config)
