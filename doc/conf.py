ce17ac92e3ee4a041df119d119513406ac67b645
cs <cs.temporary@icloud.com>
2020-03-24 17:13:19 +0000

    Set nitpicky=True to catch broken links

--- doc/conf.py
+++ doc/conf.py
@@ -40,6 +40,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Causes warnings to be thrown for all unresolvable references. This
+# will help avoid broken links.
+nitpicky = True
+
 
 # -- Options for HTML output -------------------------------------------------
 
