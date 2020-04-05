87218bcb5832c44660b1564280c78ed31a310a2f
cs <cs.temporary@icloud.com>
2020-04-05 01:53:47 +0000

    Document Convenience Kinetics
    In adding documentation to the convenience kinetics process, this commit
    also adds the ability to cite from a bibliography, which is used in the
    convenience kinetics docs.

--- doc/conf.py
+++ doc/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.bibtex',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
