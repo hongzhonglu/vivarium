d4b11edcd2913450c53a74486f93b14045e407bb
Eran Agmon <agmon.eran@gmail.com>
2020-04-22 09:51:45 -0700

    dark mass in schema

--- vivarium/processes/derive_mass.py
+++ vivarium/processes/derive_mass.py
@@ -13,44 +13,56 @@ class DeriveMass(Process):
     """
 
     defaults = {
-        'dark_mass': 0,
+        'dark_mass': 0,  # fg of dark mass
     }
 
     def __init__(self, initial_parameters={}):
 
-        self.dark_mass = initial_parameters.get(
-            'dark_mass', self.defaults['dark_mass'])
-        # TODO -- add dark mass to schema?
-
-        source_ports = initial_parameters['source_ports']
+        self.source_ports = initial_parameters['source_ports']
         target_ports = initial_parameters['target_ports']
-
         if target_ports:
             assert len(target_ports) == 1, 'DeriveMass too many target ports'
             assert list(target_ports.keys())[0] == 'global', 'DeriveMass requires target port named global'
 
-        ports = {
-            'global': ['mass', 'volume', 'mmol_to_counts']}
-        ports.update(source_ports)
+        self.dark_mass = initial_parameters.get(
+            'dark_mass', self.defaults['dark_mass'])
 
-        self.in_ports = {
-            port_id: keys
-            for port_id, keys in ports.items()
-            if port_id not in ['global']}
+        if self.dark_mass > 0:
+            self.source_ports['global'] = ['dark_mass']
+
+        # make the ports
+        ports = self.source_ports.copy()
+
+        # add global variables to global port
+        global_variables = ['mass', 'volume', 'mmol_to_counts']
+        if 'global' in ports:
+            variable_set = set(ports['global'])
+            variable_set.update(global_variables)
+            ports['global'] = list(variable_set)
+        else:
+            ports['global'] = global_variables
 
         super(DeriveMass, self).__init__(ports, initial_parameters)
 
     def default_settings(self):
-        default_state = {}
+        default_state = {
+            'global': {'dark_mass': (self.dark_mass * AVOGADRO).magnitude}
+        }
 
         # emitter keys
-        default_emitter_keys = {'global': ['mass']}
+        default_emitter_keys = {
+            'global': ['mass', 'dark_mass']}
 
         # schema
         schema = {
             'global': {
                 'mass': {
-                    'updater': 'set'}}}
+                    'updater': 'set'},
+                'dark_mass': {
+                    'mass': 1  # 1 g/mol of dark mass
+                }
+            }
+        }
 
         return {
             'state': default_state,
@@ -58,22 +70,17 @@ class DeriveMass(Process):
             'schema': schema}
 
     def next_update(self, timestep, states):
-        mass_schema = self.schema_properties(self.in_ports, 'mass')
+        mass_schema = self.schema_properties(self.source_ports, 'mass')
 
-        states_with_mass = {
-            port: molecules
-            for port, molecules in states.items()
-            if port in self.in_ports}
-
+        # get mass from all molecules in source_ports
         mass = 0
-        for port, molecules in states_with_mass.items():
-            for mol_id, count in molecules.items():
+        for port, molecules in self.source_ports.items():
+            for mol_id in molecules:
+                count = states[port][mol_id]
                 mw = mass_schema[port].get(mol_id, 0.0) * (units.g / units.mol)
                 mol = count / AVOGADRO
                 added_mass = mw * mol
                 mass += added_mass.to('fg')
-
-        mass += self.dark_mass * units.fg # TODO -- add dark mass to schema
 
         return {
             'global': {'mass': mass.magnitude}}
