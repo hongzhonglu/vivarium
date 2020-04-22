a2cd8fad52b464503d84b1d8941ad2f334230a81
Eran Agmon <agmon.eran@gmail.com>
2020-04-22 10:34:14 -0700

    remove CPM test

--- vivarium/processes/cellular_potts.py
+++ vivarium/processes/cellular_potts.py
@@ -268,7 +268,7 @@ def get_cpm_minimum_config():
         'target_area': 10,
         'animate': True}
 
-def test_CPM(cpm_config = get_cpm_minimum_config(), time=5):
+def run_CPM(cpm_config = get_cpm_minimum_config(), time=5):
     # load process
     expression = CellularPotts(cpm_config)
 
@@ -288,5 +288,5 @@ if __name__ == '__main__':
         os.makedirs(out_dir)
 
     cpm_config = get_cpm_config()
-    saved_data = test_CPM(cpm_config, 30)
+    saved_data = run_CPM(cpm_config, 30)
 
